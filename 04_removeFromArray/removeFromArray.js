166cfe1a2071091abae139a5e0aeb12cbed3048d
shivam bindroo <bindrooshivam976@gmail.com>
2024-02-02 02:25:22 +0530

    1 to 6 JS

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+   let newArr = [];
+    for(let i=0; i<array.length; i++){
+        if (!args.includes(array[i])){
+            newArr.push(array[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
